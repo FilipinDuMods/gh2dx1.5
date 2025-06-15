ded23ce63ef4ccf8087b770782d03aa2ba6b60e9
FilipinDuMatin <103686717+FilipinDuMatin@users.noreply.github.com>
2025-06-15 14:17:06 -0300

    reset

---
+++ gitinit.bat
@@ -0,0 +1,4 @@
+git init
+git remote add origin https://github.com/FilipinDuMods/gh2dx1.5.git
+git branch -M main
+git pull origin main --allow-unrelated-histories
---
+++ gitreset.bat
@@ -0,0 +1,7 @@
+rm -rf .git
+git init
+git remote add origin https://github.com/FilipinDuMods/gh2dx1.5.git
+
+git add .
+git commit -m "reset"
+git branch -M main
